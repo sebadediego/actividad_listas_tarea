a406a62fc1c8ba40951ea7ca7a7179daaa770803
Your Name <youremail@yourdomain.com>
2023-12-07 16:46:14 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+function agregarTarea() {
+    const nuevaTareaInput = document.getElementById("nuevaTarea");
+    const listaTareas = document.getElementById("listaTareas");
+    const nuevaTareaTexto = nuevaTareaInput.value.trim();
+    if (nuevaTareaTexto !== "") {
+    const nuevaTarea = document.createElement("li");
+    nuevaTarea.textContent = nuevaTareaTexto;
+    const botonEliminar = document.createElement("button");
+    botonEliminar.textContent = "Eliminar";
+    botonEliminar.className = "delete";
+    botonEliminar.onclick = function() {
+    listaTareas.removeChild(nuevaTarea);
+    };
+    nuevaTarea.appendChild(botonEliminar);
+    listaTareas.appendChild(nuevaTarea);
+    nuevaTareaInput.value = "";
+    }
+    }
+    // Función para marcar una tarea como completada
+    function marcarCompletada(tarea) {
+    tarea.classList.toggle("completed");
+    }
+    // Agregar evento de clic a las tareas para marcarlas como
+completadas
+document.getElementById("listaTareas").addEventListener("click",
+function(event) {
+if (event.target.tagName === "LI") {
+marcarCompletada(event.target);
+}
+});
